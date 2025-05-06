3bfaa5e47743c29b408f56fd31555fe31856a6ff
Ala06 <alaa.benabada@esprit.tn>
2025-05-06 16:48:13 +0100

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/elearning', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Routes
 app.use('/auth', authRoutes);
