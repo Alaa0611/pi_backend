3488e63edfae722d81a19151810132ac034ce500
Ala06 <alaa.benabada@esprit.tn>
2025-05-24 00:07:16 +0100

    Update upload.js

--- middll/upload.js
+++ middll/upload.js
@@ -1,25 +1,22 @@
-// middll/upload.js
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-
-// Define storage settings
+const uploadPath = path.join(__dirname, 'images');
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-            const dir = 'uploads/';
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
-            }
-            cb(null, dir);
-        },
-    filename: function (req, file, cb) {
-        // Generate a unique filename
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, `cover-${uniqueSuffix}${path.extname(file.originalname)}`);
+  }
 });
 
-// Create the multer instance
+
+
 const upload = multer({ storage: storage });
 
 module.exports = upload;
