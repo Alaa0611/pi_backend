15ece7f554309812307a84d494b0358760f95471
Ala06 <alaa.benabada@esprit.tn>
2025-05-24 00:09:48 +0100

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,15 +2,25 @@
 const multer = require('multer');
 const path = require('path');
 
+const fs = require('fs');
+const uploadPath = path.join(__dirname, 'images');
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'images'));
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, `cover-${uniqueSuffix}${path.extname(file.originalname)}`);
   }
 });
+
+const upload = multer({ storage });
+
 
 
 module.exports = multer({
